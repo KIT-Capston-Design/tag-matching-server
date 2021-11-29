56a906d3b4c8309bbc60aa1642c38611ba824231
jinoo7099 <vxc7099@gmail.com>
2021-11-29 19:07:04 +0900

    fix redis_init

--- redis_init.py
+++ redis_init.py
@@ -3,5 +3,5 @@ import redis
 host = "KITCapstone.iptime.org"
 port = "6379"
 
-# 레디스 연결
+# connect redis
 rd = redis.StrictRedis(host=host, port=port, db=0, password="8788", charset="utf-8", decode_responses=True)
